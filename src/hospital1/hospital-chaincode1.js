ce1791195cc629d1b2fd1bf75b51c1ac51e683b1
ABDUKODIROV FARKHOD <abdukodirovfarkhod@gmail.com>
2023-06-10 19:44:31 +0900

    Add files via upload

---
+++ src/hospital1/hospital-chaincode1.js
@@ -0,0 +1,24 @@
+// hospital-chaincode1.js
+
+async function addMedicalRecord(ctx, patientId, medicalRecord) {
+    // Store the medical record for the patient
+    await ctx.stub.putState(patientId, Buffer.from(JSON.stringify(medicalRecord)));
+    console.log('Medical record added:', medicalRecord);
+  }
+  
+  async function getMedicalRecord(ctx, patientId) {
+    // Retrieve the medical record for the patient
+    const medicalRecordBytes = await ctx.stub.getState(patientId);
+    if (!medicalRecordBytes || medicalRecordBytes.length === 0) {
+      throw new Error(`Medical record for patient ${patientId} does not exist`);
+    }
+    const medicalRecord = JSON.parse(medicalRecordBytes.toString());
+    console.log('Medical record retrieved:', medicalRecord);
+    return medicalRecord;
+  }
+  
+  module.exports = {
+    addMedicalRecord,
+    getMedicalRecord,
+  };
+  
---
+++ src/hospital2/hospital-script.js
@@ -0,0 +1,30 @@
+// hospital-script.js
+
+const { FileSystemWallet, Gateway } = require('indy-network');
+const path = require('path');
+
+async function main() {
+  const ccpPath = path.resolve(__dirname, '..', 'connection.json');
+  const walletPath = path.resolve(__dirname, '..', 'wallet');
+
+  const wallet = new FileSystemWallet(walletPath);
+
+  const gateway = new Gateway();
+  await gateway.connect(ccpPath, {
+    wallet,
+    identity: 'hospital1',
+    discovery: { enabled: true, asLocalhost: true },
+  });
+
+  const network = await gateway.getNetwork('mychannel');
+  const contract = network.getContract('hospital-chaincode2');
+
+  // Invoke chaincode functions here
+}
+
+main().then(() => {
+  console.log('Script completed successfully');
+}).catch((error) => {
+  console.error(`Script execution failed: ${error}`);
+  process.exit(1);
+});
